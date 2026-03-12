13f91e740d77a4c94b3e61ce1ad8f51d329ed78a
Maria19761976 <mpotero1976@gmail.com>
2026-03-12 08:44:11 +0100

    feat: add App component with routing configuration

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import HomePage from './pages/HomePage/HomePage';
+import CharacterDetailPage from './pages/CharacterDetailPage/CharacterDetailPage';
+import './App.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/students" element={<HomePage />} />
+        <Route path="/staff" element={<HomePage />} />
+        <Route path="/houses" element={<HomePage />} />
+        <Route path="/character/:name" element={<CharacterDetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
